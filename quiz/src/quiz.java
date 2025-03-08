2f909de4d0dbd00af5c331714b6a3fe39a7c1431
JHES561 <thejhes2006@gmail.com>
2025-03-08 12:45:59 -0500

    Add files via upload

---
+++ quiz/src/quiz.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class quiz {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in)
+
+        System.out.println("ingrese las horas");
+        int numero1 = scanner.nextInt();
+        if (numero1= 0 && 24){
+            System.out.println("ingrese los minutos");
+            }
+        }else {
+            System.out.println("ERROR");
+        }
+        scanner.close();
+    }
+
